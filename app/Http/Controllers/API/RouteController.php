223d6511a02352b3a4d79dddcd56551511596c09
ZawMyoThantSin <zawmyothantzin37@gmail.com>
2024-08-21 15:44:29 +0630

    add api v.2

--- app/Http/Controllers/API/RouteController.php
+++ app/Http/Controllers/API/RouteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class RouteController extends Controller
@@ -19,4 +20,29 @@ class RouteController extends Controller
         $categories = Category::get();
         return response()->json($categories,200);
     }
+
+    public function createCategory(Request $request){
+        $data = [
+            'name' => $request->name,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ];
+        $response = Category::create($data);
+        return response()->json($response,200);
+    }
+    public function deleteCategory($id){
+        $data = Category::where('id',$id)->first();
+        if(!empty($data)){
+            Category::where('id',$id)->delete();
+            return response()->json([
+                'status' => '200, OK',
+                'message' => 'Category Deleted Successfully...'
+            ],200);
+        }
+        return response()->json([
+            'status' => '404, NOT Found',
+            'message' => 'There is no Category with that ID...'
+        ],404);
+    }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,12 @@
 use App\Http\Controllers\API\RouteController;
 use Illuminate\Support\Facades\Route;
 
+
+// GET
 Route::get('product/list',[RouteController::class,'productList']);
 Route::get('category/list',[RouteController::class,'categoryList']);
+Route::get('category/delete/{id}',[RouteController::class, 'deleteCategory']);
+
+// POST
+Route::post('category/create',[RouteController::class,'createCategory']);
+
